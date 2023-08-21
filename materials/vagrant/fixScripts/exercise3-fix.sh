4ebb990a5d596b2223424a63f94d1b8ddedcf83c
Maziar <kfir.maziar@philips.com>
2023-08-21 16:52:47 +0300

    added fix for exercise 3

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 #add fix to exercise3 here
+#
+
+config_file="/etc/apache2/sites-enabled/000-default.conf"
+line_number=33
+word_to_find="denied"
+replacement_word="granted"
+
+sed -i "${line_number}s/$word_to_find/$replacement_word/" "$config_file"
+
+sudo systemctl reload apache2
+
